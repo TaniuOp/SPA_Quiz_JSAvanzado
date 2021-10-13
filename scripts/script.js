95c52bec1f0ab3c8351b32f4356e262596b09251
TaniuOp <taniuop@gmail.com>
2021-10-13 20:58:57 +0200

    ajuste script

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,53 @@
-// FIREBASE  
+//Creamos variable para indicar el elemento donde irá la pregunta 
+const questionDiv =  document.getElementsByClassName("questionSection")[0];
+
+// Obtenemos las preguntas de la API 
+let questionsArray;
+const getQuestions = async () => {
+    try{
+        let questionsUrl = await fetch('https://opentdb.com/api.php?amount=10&category=18&type=multiple')
+        let questionsJson = await questionsUrl.json()
+        questionsArray= questionsJson.results
+        console.log (questionsArray)
+        return questionsArray //nos devuelve un array con 10 preguntas. Debemos obtener data "question"
+    }
+    catch (error) {
+        console.log("we had an error obtaining the data")
+    }
+}
+getQuestions() 
+
+// mapear cada pregunta, en c/u iteracción. Crear elemento "pregunta" . Sigu. iteracc, elimino nodos y pongo nuevos (.map)
+
+const getQuestionData = () => {
+    questionsArray.map(async(element) => {  //usando map se puede evitar el for 
+        let question = await element.question //accedo a la data dentro del elemento 
+        // let allQuestion= await [question]    
+        console.log(question)        //metemos la question en el h3 
+    })
+    return question
+    }
+getQuestionData()
+
+// Imprime la pregunta en el HTML
+// async function showQuestion(){
+//     questionTexts.innerText = await questions;
+// }
+// showQuestion()
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // FIREBASE  
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -24,55 +73,9 @@ await setDoc(doc(db, "Halloween", "User"), {
     errors: "7"
 });
 //recoger info de interacción y crear variable (para quiz)
-
-
-
-// QUIZ 
-const q = query(collection(db, "Halloween"));
-
-const querySnapshot = await getDocs(q);
-querySnapshot.forEach((doc) => {
-  console.log(doc.id, " => ", doc.data());
-});
-
-
-const questionDiv =  document.getElementsByClassName("questionSection")[0];
-
-// Obtenemos las preguntas de la API 
-let questions;
-const getQuestions = async () => {
-    try{
-        let questionsUrl = await fetch('https://opentdb.com/api.php?amount=10&category=18&type=multiple')
-        let questionsJson = await questionsUrl.json()
-        questions= questionsJson.results
-        console.log (questions)
-        return questions 
-    }
-    catch (error) {
-        console.log("we had an error obtaining the data")
-    }
-}
-getQuestions()
-
-// mapear cada pregunta, en c/u iteracción. Crear elemento "pregunta" . Sigu. iteracc, elimino nodos y pongo nuevos (.map)
-// const questionTexts =  document.createElement("h3");
-// questionDiv.appendChild(questionTexts);
-
-// // Imprime la pregunta en el HTML
-// async function showQuestion(){
-//     questionTexts.innerText = await questions;
-// }
-// showQuestion()
-
-
-
-/*const getData = () => {
-    dataTransportItems.map(async (element) => {  //usando map se puede evitar el for 
-        let latitude = await element.latitude //accedo a la data dentro del elemento (declaro latitud)
-        let longitude = await element.longitude //declaro longitud. Pdte refactorizar con destructuring
-        let id = await element.id
-        let allLocations= await [latitude, longitude]      //creamos un array de arrays 
-        L.marker(allLocations).addTo(map); //hacemos un push del array (lat & long)
-        L.marker(allLocations).bindPopup(id).addTo(map); //Con un popup, dibujar el ID del vehículo
-    })
-    }*/
+// Firebase QUIZ 
+// const q = query(collection(db, "Halloween"));
+// const querySnapshot = await getDocs(q);
+// querySnapshot.forEach((doc) => {
+//   console.log(doc.id, " => ", doc.data());
+// });
