7f6a6608b9ee6dc888ecb83e0a1bfdcb70586385
TaniuOp <taniuop@gmail.com>
2021-10-15 09:37:18 +0200

    obtención del objeto con su pregunta, respuesta correcta e incorrectas. Modificación de textos en ingles (para acostumbrarnos). Añadidos comentarios que indican el flujo de las funciones

--- scripts/script.js
+++ scripts/script.js
@@ -1,81 +1,51 @@
 //Creamos variable para indicar el elemento donde irá la pregunta 
 const questionDiv =  document.getElementsByClassName("questionSection")[0];
-
+let allData={}
 // Obtenemos las preguntas de la API 
-let questionsArray;
+let questionsArray=[];
 const getQuestions = async () => {
     try{
         let questionsUrl = await fetch('https://opentdb.com/api.php?amount=10&category=18&type=multiple')
         let questionsJson = await questionsUrl.json()
-        questionsArray= questionsJson.results
-        console.log (questionsArray)
-        return questionsArray //nos devuelve un array con 10 preguntas. Debemos obtener data "question"
+        questionsArray= questionsJson.results //nos devuelve un array con 10 preguntas. 
+        let arrayQuestions =  []
+        let arrayBadAnswers =  []
+        let arrayGoodAnswers =  []
+        for (let i = 0; i< questionsArray.length; i++){ 
+            arrayQuestions.push(questionsArray[i].question)
+            arrayBadAnswers.push(questionsArray[i].incorrect_answers)
+            arrayGoodAnswers.push(questionsArray[i].correct_answer)
+        }
+        //obtenemos un objeto con su pregunta, respuesta incorrecta y respuesta correcta 
+        allData = { 
+            TheQuestion: arrayQuestions,
+            TheBadAnswers: arrayBadAnswers,
+            TheGoodAnswer: arrayGoodAnswers
+        }
+        console.log(allData)
+        return allData
     }
     catch (error) {
         console.log("we had an error obtaining the data")
     }
 }
-getQuestions() 
+//questionNumber será el numero de la pregunta donde estemos (es decir, el index del array +1)[0]
+const getQuestionData = async (questionNumber) => {
+    let myQuestion = await allData.TheQuestion[questionNumber]
+    let myBadAnswers = await allData.TheBadAnswers[questionNumber]
+    let myGoodQuestion = await allData.TheGoodAnswer[questionNumber]
+    let allTheQuestionData= [myQuestion, myBadAnswers, myGoodQuestion]
+    console.log(allTheQuestionData)
+    return allTheQuestionData // devuelve toda la info necesaria para montar la pantalla de pregunta y respuestas (pregunta, 3 respuestas incorrectas y una correcta)
+}
 
-// mapear cada pregunta, en c/u iteracción. Crear elemento "pregunta" . Sigu. iteracc, elimino nodos y pongo nuevos (.map)
+async function startData(){
+    await getQuestions() 
+    await getQuestionData(6) //este num si lo cambiamos dinamicamente envía la pregunta del array que corresponde con su idex 
+}
+startData() //iniciamos las funciones anteriormente declaradas 
 
-const getQuestionData = () => {
-    questionsArray.map(async(element) => {  //usando map se puede evitar el for 
-        let question = await element.question //accedo a la data dentro del elemento 
-        // let allQuestion= await [question]    
-        console.log(question)        //metemos la question en el h3 
-    })
-    return question
-    }
-getQuestionData()
-
-// Imprime la pregunta en el HTML
-// async function showQuestion(){
-//     questionTexts.innerText = await questions;
-// }
-// showQuestion()
+//ideal sería que la funcion getQuestionData se llamara con el botón de "Siguiente" y enviará como parametro el numero de la siguiente pregunta 
 
 
 
-
-
-
-
-
-
-
-
-
-
-    // FIREBASE  
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-import { getFirestore, setDoc, doc, collection, getDocs, query } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js"
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA-WeyBkPMXLHw3PDyM_kpQ9OGQ5f-30Cw",
-  authDomain: "quizspa-d230e.firebaseapp.com",
-  projectId: "quizspa-d230e",
-  storageBucket: "quizspa-d230e.appspot.com",
-  messagingSenderId: "846878938512",
-  appId: "1:846878938512:web:413ff20c7a99d53abf8f15"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-await setDoc(doc(db, "Halloween", "User"), {
-    name: "Taniu",
-    goodQuestions: "3",
-    errors: "7"
-});
-//recoger info de interacción y crear variable (para quiz)
-// Firebase QUIZ 
-// const q = query(collection(db, "Halloween"));
-// const querySnapshot = await getDocs(q);
-// querySnapshot.forEach((doc) => {
-//   console.log(doc.id, " => ", doc.data());
-// });
